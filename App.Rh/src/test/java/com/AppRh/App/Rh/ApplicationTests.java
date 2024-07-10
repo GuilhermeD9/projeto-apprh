b37838eb39b1d5e687817b085565a37b8479e113
Guilherme Silva <sgsouza.173@gmail.com>
2024-07-10 15:11:17 -0300

    Atualizando o repositório

---
+++ App.Rh/src/test/java/com/AppRh/App/Rh/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppRh.App.Rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
